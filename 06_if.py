a821d0a65229f01d1b19997eff012f2b9932ea22
Robert Vari <mail.robertvari@gmail.com>
2023-08-14 11:37:48 +0200

    update

---
+++ 06_if.py
@@ -0,0 +1,25 @@
+number = 12
+
+#     False
+if number == 10:
+    print("The number is 10.")
+
+#     True
+if number != 10:
+    print("The number is not 10.")
+
+#     False
+if number < 10:
+    print("The number is less the 10.")
+
+#     True
+if number > 10:
+    print("The number is greater than 10.")
+
+#     False
+if number <= 10:
+    print("The number is less or equal to 10.")
+
+#     True
+if number >= 10:
+    print("The number is greater or equal to 10.")
