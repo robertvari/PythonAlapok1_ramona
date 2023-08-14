8bcaadd250ebe18f078c75b881144118c728b918
Robert Vari <mail.robertvari@gmail.com>
2023-08-14 10:34:29 +0200

    update

---
+++ 03_variables.py
@@ -0,0 +1,6 @@
+name = "Vári Róbert"
+address = "Budapest"
+email = "mail.pythonsuli@gmail.com"
+age = 46
+
+print(name, address, email, age)
---
+++ 04_string_format.py
@@ -0,0 +1,6 @@
+name = "Kovács Balázs"
+address = "Pécs"
+email = "mail.pythonsuli@gmail.com"
+age = 46
+
+print(f"My name is {name}. I live in {address}. My email is {email}. I'm {age} years old.")
