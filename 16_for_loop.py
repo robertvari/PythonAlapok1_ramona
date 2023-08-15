8cb53ec65afde432f1caa6c3c3a722dad86b543b
Robert Vari <mail.robertvari@gmail.com>
2023-08-15 10:08:28 +0200

    update

---
+++ 16_for_loop.py
@@ -0,0 +1,7 @@
+my_friends = ["Csaba", "Kriszta", "Tamás", "Géza", "Bálint", "Csilla"]
+fruits = ["apple", "lemon", "orange", "avocado", "banana"]
+numbers = [1, 2, 3, 4, 5, 6]
+
+
+for item in numbers:
+    print(item)
