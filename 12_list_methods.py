6e0a0375148fa94850c797c67c5a09f35a46415f
Robert Vari <mail.robertvari@gmail.com>
2023-08-15 08:42:50 +0200

    update

---
+++ 12_list_methods.py
@@ -0,0 +1,22 @@
+#                0         1         2
+my_friends = ["Csaba", "Kriszta", "Tamás"]
+
+# add item
+# my_friends.append("Balázs")
+# my_friends.append("Csilla")
+
+new_friend = input("Who is your friend?")
+my_friends.append(new_friend)
+print(my_friends)
+
+# my_friends.insert(2, "Ica")
+
+# remove item
+# my_friends.remove("Tamás")
+# del my_friends[0]
+
+
+# update item
+# my_friends[1] = "Csilla"
+# my_friends[-1] = "Géza"
+# print(my_friends)
