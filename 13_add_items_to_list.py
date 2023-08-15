41ccecf5570c7e3d495571e6429df32239babf0d
Robert Vari <mail.robertvari@gmail.com>
2023-08-15 08:56:17 +0200

    update

---
+++ 13_add_items_to_list.py
@@ -0,0 +1,13 @@
+my_favorite_bands = []
+
+favorite_band = input("Who is your favorite band?")
+my_favorite_bands.append(favorite_band)
+print(my_favorite_bands)
+
+favorite_band = input("Who is your favorite band?")
+my_favorite_bands.append(favorite_band)
+print(my_favorite_bands)
+
+favorite_band = input("Who is your favorite band?")
+my_favorite_bands.append(favorite_band)
+print(my_favorite_bands)
