718c0f42337208e61f23a3cff57391060499e9f2
Robert Vari <mail.robertvari@gmail.com>
2023-08-15 08:15:36 +0200

    update

---
+++ 11_lists.py
@@ -0,0 +1,5 @@
+#                0         1         2        3        4         5
+#                                             -3      -2        -1
+my_friends = ["Csaba", "Kriszta", "Tamás", "Géza", "Bálint", "Csilla"]
+
+print(my_friends[1])
