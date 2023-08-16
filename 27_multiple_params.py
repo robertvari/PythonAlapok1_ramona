05310fe94906ed98682cbd2157847a739626e6e6
Robert Vari <mail.robertvari@gmail.com>
2023-08-16 11:44:48 +0200

    update

--- 27_multiple_params.py
+++ 27_multiple_params.py
@@ -1,3 +1,9 @@
 # function definition
-def say_hello(name):
+def say_hello(name, email, address):
     print(f"Hello {name}")
+    print(f"Your email is: {email}")
+    print(f"You address is: {address}")
+
+# keyworld param
+say_hello(email="csaba@gmail.com", address="Eger", name="Csaba")
+say_hello("Csaba", "csaba@gmail.com", "Eger")
