b4773b0fc6073a089a0311e517a7e527486cf43a
Robert Vari <mail.robertvari@gmail.com>
2023-08-16 11:20:26 +0200

    update

---
+++ 24_min_max.py
@@ -0,0 +1,7 @@
+lista = [12, 5, 4, 8, 9, 11, 10, 12, 6]
+
+minimum = min(lista)
+maximum = max(lista)
+
+print(f"The minimum number is: {minimum}")
+print(f"The maximum number is: {maximum}")
---
+++ 25_a_function.py
@@ -0,0 +1,15 @@
+import time
+
+# def = definition
+# function definition
+def coffee_maker():
+    print("Making a coffee!")
+    print("...")
+    time.sleep(3)
+
+    print("Your coffee is ready.")
+    time.sleep(1)
+    print("Enjoy :)")
+
+# call a function
+coffee_maker()
