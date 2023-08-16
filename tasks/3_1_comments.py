7932bb27729d15f1eff0f65162e09955d94a26be
Robert Vari <mail.robertvari@gmail.com>
2023-08-16 09:30:20 +0200

    update

---
+++ tasks/3_1_comments.py
@@ -0,0 +1,11 @@
+# My name is Robert Vari
+
+"This is a string" # string
+
+"""
+This is a 
+multilie 
+comment
+"""
+
+print("Hello World")
---
+++ tasks/4_1_is_a_good_day.py
@@ -0,0 +1,6 @@
+is_a_good_day = input("Is it a good day? (yes/no)")
+
+if is_a_good_day == "yes":
+    print("I'm glad you're having a nice day :)")
+else:
+    print("Sorry to hear that :(")
