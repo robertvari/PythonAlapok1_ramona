4c84409a4bb708baf0160452e456c9ddc2b69ee1
Robert Vari <mail.robertvari@gmail.com>
2023-08-17 10:57:27 +0200

    update

---
+++ 31_nested_lists.py
@@ -0,0 +1,7 @@
+robert_notes = [1, 2, 3, 1, 1, 2]
+ramona_notes = [4, 5, 3, 4, 5]
+csaba_notes = [3, 4, 4, 3, 5, 5, 4, 4, 4]
+kriszta_notes = [5, 4, 4, 5, 5, 5, 4, 5]
+
+student_list = [robert_notes, ramona_notes, csaba_notes, kriszta_notes]
+print(student_list[0][0])
