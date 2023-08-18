689da8d37b9b3442adde6946a5da231fdc89e7b6
Robert Vari <mail.robertvari@gmail.com>
2023-08-18 11:00:31 +0200

    update

---
+++ tasks/5_6_randum_number_list.py
@@ -0,0 +1,18 @@
+import random
+
+random_numbers = []
+
+# generate 20 random numbers
+count = 0
+while count < 20:
+    count += 1
+    random_numbers.append(random.randint(1,12))
+
+# print all numbers that can be divided by 3
+found = 0
+for item in random_numbers:
+    if item % 3 == 0:
+        print(item)
+        found += 1
+
+print(f"We found {found} numbers that can be divided with 3")
