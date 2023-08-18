471797ea189700c41950a36d4a4760e206819e56
Robert Vari <mail.robertvari@gmail.com>
2023-08-18 09:14:22 +0200

    update

---
+++ tasks/3_2_1_print_circle_data.py
@@ -0,0 +1,12 @@
+"""
+This program calculates the Circumference and Area of a circle.
+"""
+
+pi = 3.14
+radius = 4
+
+circumference = (2*pi) * radius
+area = pi*(radius*radius)
+
+print(f"The circumference of the circle with radius {radius} is: {circumference}")
+print(f"The area of the circle with radius {radius} is: {area}")
