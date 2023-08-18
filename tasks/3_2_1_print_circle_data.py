d48808de348fa8f0bd07dd63f4434c9ef9637006
Robert Vari <mail.robertvari@gmail.com>
2023-08-18 09:29:02 +0200

    update

--- tasks/3_2_1_print_circle_data.py
+++ tasks/3_2_1_print_circle_data.py
@@ -6,7 +6,7 @@ pi = 3.14
 radius = 4
 
 circumference = (2*pi) * radius
-area = pi*(radius*radius)
+area = pi*(radius**2)
 
 print(f"The circumference of the circle with radius {radius} is: {circumference}")
 print(f"The area of the circle with radius {radius} is: {area}")
