78c6e32ec4ddd636ecdef2be9f605dd59adb9d21
Robert Vari <mail.robertvari@gmail.com>
2023-08-19 10:01:15 +0200

    update

---
+++ tasks/6_3_get_world2.py
@@ -0,0 +1,14 @@
+user_input = input("Give me a word which starts with a letter 'a':")
+
+word_list = []
+
+while len(user_input) != 0:
+    if user_input[0].lower() == "a":
+        word_list.append(user_input)
+        print(f"Thank you. I added you word {user_input} to my list.")
+    else:
+        print("Incorrect world.")
+
+    user_input = input("Give me a word which starts with a letter 'a':")
+
+print(f"You words: {word_list}")
